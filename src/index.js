429dbc3778cd627e649cfd0a3c25bb7ff7966607
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-24 16:28:56 +0530

    Update Webpack

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import _ from 'lodash';
 import './style.css';
 
-
-
-function component() {
-
-
-  const li = document.createElement('li');
-  const todo = document.getElementById('task-input').value
-
-  li.innerHTML = `<li><input type="checkbox"/> - ${todo}</li>`;
-
-  return li;
-}
-
-const btn = document.getElementById('btn')
-
-btn.addEventListener('click', () => {
-  document.querySelector('#list-container').appendChild(component());
-});
+console.log('I hav arrived...');
